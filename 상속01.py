ca28195b8788e7aa3479fa7b6637ae21f69553fe
seohyang0 <ksk076@naver..com>
2023-08-08 16:40:53 +0900

    클래스 연습 완료

--- 상속01.py
+++ 상속01.py
@@ -7,16 +7,18 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
 class Student(Person):
+    #메서드 재정의(덮어쓰기)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #직접 부모클래스의 초기화 메소드
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #메서드 재정의
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
-
-
+p.printInfo()
+s.printInfo()
